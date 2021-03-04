b35d61f909a3f057726409848879772cf1685911
RodrigoLS <rolima13@gmail.com>
2021-03-04 17:40:26 -0300

    layout feed page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
-import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
+
+import { MainComponent } from './pages/main/main.component';
+import { FeedComponent } from './pages/main/feed/feed.component';
+import { ProfileComponent } from './pages/main/profile/profile.component';
+import { FriendsComponent } from './pages/main/friends/friends.component';
+import { SearchComponent } from './pages/main/search/search.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
-  { path: 'profile', component: ProfileComponent}
+
+  { path: 'dashboard', component: MainComponent, children: 
+    [
+      { path: '', component: FeedComponent },
+      { path: 'profile/:id', component: ProfileComponent },
+      { path: 'friends', component: FriendsComponent },
+      { path: 'search', component: SearchComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileComponent } from './pages/main/profile/profile.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostComponent } from './components/post/post.component';
 import { SendPostComponent } from './components/send-post/send-post.component';
 import { CommentComponent } from './components/comment/comment.component';
+import { MainComponent } from './pages/main/main.component';
+import { FeedComponent } from './pages/main/feed/feed.component';
+import { FriendsComponent } from './pages/main/friends/friends.component';
+import { SearchComponent } from './pages/main/search/search.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { CommentComponent } from './components/comment/comment.component';
     NavbarComponent,
     PostComponent,
     SendPostComponent,
-    CommentComponent
+    CommentComponent,
+    MainComponent,
+    FeedComponent,
+    FriendsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
